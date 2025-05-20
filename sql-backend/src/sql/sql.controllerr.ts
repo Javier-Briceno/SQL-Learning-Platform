0df42474bd4f30a099ac1bfc4dfa94f9c6875583
Alexander Riedle <ARiedle57@gmail.com>
2025-05-20 17:53:01 +0200

    Angepasster Controller für die Post Anfrage des Sql Uploads

--- sql-backend/src/sql/sql.controllerr.ts
+++ sql-backend/src/sql/sql.controllerr.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { SqlService } from './sql.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Multer } from 'multer';
 
 @Controller('sql') 
-export class SqlController {}
+export class SqlController {
+
+   constructor(private readonly sqlService: SqlService) {}
+
+@Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async importSqlFile(@UploadedFile() file: Multer.File) {
+    const sql = file.buffer.toString('utf8');
+  }
+}
