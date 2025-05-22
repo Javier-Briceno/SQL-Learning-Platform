26a567bb249dfdf00f2138a6b93551d12e9d877e
Alexander Riedle <ARiedle57@gmail.com>
2025-05-22 17:30:52 +0200

    Auslesen des SQL Files nach CREATE DATABASE

--- sql-backend/src/sql/sql.service.ts
+++ sql-backend/src/sql/sql.service.ts
@@ -13,5 +13,20 @@ export class SqlService {
             .map((s) => s.trim())
             .filter((s) => s.length > 0);
 
+        // Überprüfen, ob das erste Statement ein CREATE DATABASE ist
+        const createDbStmt = statements.shift();
+
+        if (!createDbStmt) {
+            throw new Error('Keine SQL-Statements gefunden.');
+        }
+
+        const dbNameMatch = createDbStmt.match(/CREATE DATABASE\s+([^\s;]+)/i);
+
+        if (!dbNameMatch) {
+            throw new Error('CREATE DATABASE fehlt oder ist ungültig.');
+        }
+
+        const newDbName = dbNameMatch[1];
+
     }
 }
