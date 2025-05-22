2dcc41d52afaf8a0ea3214ba1cfcda5058a3caba
Kewin Hudalla <khudalla@web.de>
2025-05-22 16:22:48 +0200

    Profil Icon nicht mehr für login und register sichtbar.

--- sql-frontend/src/app/app.component.ts
+++ sql-frontend/src/app/app.component.ts
@@ -1,21 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { RouterOutlet, RouterModule, Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { AuthService } from './auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, MatIconModule, MatButtonModule, MatMenuModule],
+  imports: [RouterOutlet, RouterModule, MatIconModule, MatButtonModule, MatMenuModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  public currentUrl = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {
+    this.currentUrl = this.router.url;
+    this.router.events.subscribe(() => {
+      this.currentUrl = this.router.url;
+    });
+  }
 
   logout() {
     this.authService.logout();
